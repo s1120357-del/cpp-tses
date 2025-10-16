2b1ee6593b0874360ef525e76a7b1ba3c116aba4
s1120357-del <s1120357@dmhs.kh.edu.tw>
2025-10-16 06:27:26 +0000

    HW5

--- test1/main.cpp
+++ test1/main.cpp
@@ -2,10 +2,22 @@
 using namespace std;
 
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0)
-            cout << i << " ";
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    int i = 1;
+    while (i <= 9) {
+        int j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j++;
+        }
+        cout << endl;
+        i++;
     }
     return 0;
 }
