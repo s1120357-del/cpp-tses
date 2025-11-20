ba91ab45f5f092aee86bc59bdfeaa384aed31c61
s1120357-del <s1120357@dmhs.kh.edu.tw>
2025-11-20 06:21:05 +0000

    hw9

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    for (int i = 1; i <= 6; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << "*";
-        }
-        cout << endl;
-    }
+int accumulate(int n);
 
-    int i = 1;
-    while (i <= 9) {
-        int j = 1;
-        while (j <= 9) {
-            cout << i << " * " << j << " = " << i * j << "\t";
-            j++;
-        }
-        cout << endl;
-        i++;
-    }
+int main() {
+    int number = 100;  // Set the upper limit of the sum
+    cout << "從1加到" << number << "總合為: " << accumulate(number);  // Display the result
     return 0;
+}
+
+// Recursive function to calculate the sum
+int accumulate(int n) {
+    if (n == 1) {
+        return 1;  // Base case: return 1 when n reaches 1
+    }
+    return n + accumulate(n - 1);  // Recursive case: sum up the numbers
 }
